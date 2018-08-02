14fcad3b72eff2099a5172897ad840c6ce85fc6f
Donovan T. Le <dtle647@gmail.com>
2018-08-01 20:10:09 -0700

    fix: start of the screen scrapper

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -1,6 +1,22 @@
 from unittest import TestCase
 
+# import urllib2
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 class TestFib(TestCase):
     def test_fib(self):
         assert True
+
+    def test_scrap_some_data(self):
+        print("Hi!")
+        quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
+        page = urlopen(quote_page)
+
+        soup = BeautifulSoup(page, 'html.parser')
+
+        shit_bird = soup.find('h1')
+        shit_bird.get_text()
+        assert shit_bird.get_text() == "Terms of Service Violation"
+
+        print(shit_bird.get_text())
