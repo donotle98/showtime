53c980636fd593a719c2d305a64fefd46a4ce718
Donovan T. Le <dtle647@gmail.com>
2018-08-02 17:00:59 -0700

    This is supposed to be listing genre types of shows/movies. PLease fix

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 
 # import urllib2
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+
 
 class TestFib(TestCase):
     def test_fib(self):
@@ -20,3 +22,12 @@ class TestFib(TestCase):
         assert shit_bird.get_text() == "Terms of Service Violation"
 
         print(shit_bird.get_text())
+
+        api_key = '881a51710c249ba3a6cf179cb54bb437'
+        page_link = 'http://api.tvmedia.ca/tv/v4/genres/movies?api_key=881a51710c249ba3a6cf179cb54bb437'
+        from pip._vendor.urllib3 import response
+        page_response = response.(page_link, timeout=)
+        page_content = BeautifulSoup(page_response.content, "html.parser")
+
+        show_data = page_content.find_all('genre')
+        print(show_data)
